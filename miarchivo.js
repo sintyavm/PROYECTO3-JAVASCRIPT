5f5b9426c77a436aade3e79f474348eab3976f8e
sindi <sindi.vitulas@gmail.com>
2023-07-27 10:40:43 -0500

    otra vez  nuevos cambios

--- miarchivo.js
+++ miarchivo.js
@@ -118,11 +118,29 @@ const pintarCarrito =() =>{
            <img src="${product.img}">
            <h3>${product.nombre}</h3>
            <p>${product.precio} $</p>
+           <span class="restar"> - </span>
            <p>Cantidad: ${product.cantidad}</p>
+           <span class="sumar"> + </span>
            <p>Total: ${product.cantidad * product.precio}</p>
         `;
 
         modalContainer.append(carritoContent);
+
+        let restar = carritoContent.querySelector(".restar");
+        restar.addEventListener("click", ()=>{
+            if(product.cantidad !== 1) {
+                product.cantidad--;
+            }
+            saveLocal();
+            pintarCarrito();
+        });
+
+        let sumar = carritoContent.querySelector(".sumar");
+        sumar.addEventListener("click", ()=>{
+                product.cantidad++;
+            saveLocal();
+            pintarCarrito();    
+        });
 
         console.log(carrito.length);
 
@@ -150,10 +168,14 @@ const eliminarProducto = () => {
         return carritoId !== foundId;
     });
     carritoCounter();
+    saveLocal();
     pintarCarrito();
 };
 
 const carritoCounter = () =>{
     cantidadCarrito.style.display = "block";
-    cantidadCarrito.innerText = carrito.length;
+    const carritoLength = carrito.length;
+    localStorage.setItem("carritoLength", JSON.stringify(carritoLength));
+    cantidadCarrito.innerText =  JSON.parse(localStorage.getItem("carritoLength"));
 };
+    carritoCounter();
